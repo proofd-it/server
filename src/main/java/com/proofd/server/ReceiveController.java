eb8ffbab20c8e634cddb386bf4ee143d2fbd4bd8
devon96 <kdryja@gmail.com>
2020-01-17 21:54:12 +0000

    rewrite webapp in java

--- src/main/java/com/proofd/server/ReceiveController.java
+++ src/main/java/com/proofd/server/ReceiveController.java
@@ -1,7 +1,21 @@
 package com.proofd.server;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.*;
+import org.apache.http.util.EntityUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,47 +32,50 @@ public class ReceiveController {
 
     public static final String owner = "MestonIndustries";
     public static final String commodity = "DeliveryItem";
+    public static final String apiEndpoint = "https://trustlens.abdn.ac.uk/doors/api/Delivery";
 
-    @PostMapping(value = "/")
+    @PostMapping(value = "/save")
     @ResponseStatus(HttpStatus.OK)
-    public String receiveData(@RequestBody Map<String, Object> rawPayload) throws IOException {
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        FileOutputStream out = new FileOutputStream(String.format("/var/log/proofdit/%s", timestamp).replace(" ", "_"));
-        String payload = rawPayload.toString();
-        out.write(payload.getBytes());
-        out.close();
+    public void addToBlockchain(@RequestBody String rawPayload) throws IOException {
+        JsonObject req = new JsonObject();
+        req.addProperty("complianceReport", rawPayload);
+        req.addProperty("status", "accepted");
+        req.addProperty("newOwner", owner);
+        req.addProperty("commodity", commodity);
 
-        System.out.println(new Gson().toJson(payload));
+        System.out.println(req.toString());
 
-        URL url = new URL("https://trustlens.abdn.ac.uk/doors/api/Delivery");
-        HttpURLConnection con = (HttpURLConnection)url.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json; utf-8");
-        con.setRequestProperty("Accept", "application/json");
-        String username = System.getProperty("APP_MASTER_USERNAME");
-        String password = System.getProperty("APP_MASTER_PASSWORD");
-        con.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString((username+":"+password).getBytes()));
-        con.setDoOutput(true);
+        CredentialsProvider provider = new BasicCredentialsProvider();
+        provider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials(System.getProperty("APP_MASTER_USERNAME"),
+                        System.getProperty("APP_MASTER_PASSWORD"))
+        );
 
-        try(OutputStream os = con.getOutputStream()) {
-            byte[] input = payload.getBytes("utf-8");
-            os.write(input, 0, input.length);
+        CloseableHttpClient httpclient = HttpClientBuilder.create()
+                .setDefaultCredentialsProvider(provider)
+                .build();
+        HttpPost httpPost = new HttpPost(apiEndpoint);
+        HttpEntity stringEntity = new StringEntity(req.toString(), ContentType.APPLICATION_JSON);
+        httpPost.setEntity(stringEntity);
+        CloseableHttpResponse response = httpclient.execute(httpPost);
+        HttpEntity entity = response.getEntity();
+        String responseBody = EntityUtils.toString(entity);
+
+        System.out.println(responseBody);
+    }
+
+    @PostMapping(value = "/transaction")
+    @ResponseStatus(HttpStatus.OK)
+    public void saveData(@RequestBody String payload) throws IOException {
+        try {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            FileOutputStream out = new FileOutputStream(String.format("/var/log/proofdit/%s", timestamp).replace(" ", "_"));
+            out.write(payload.getBytes());
+            out.close();
         }
-
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
-            }
-            System.out.println(response.toString());
+        catch (IOException exc) {
+            System.err.println("ERROR WRITING: " + exc.getMessage());
         }
-        catch(IOException e) {
-            System.err.println(e);
-            System.err.println(IOUtils.toString(con.getErrorStream(), "utf-8"));
-        }
-
-        return "success";
     }
 }
